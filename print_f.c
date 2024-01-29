6ece21104a8045ebf0e5115420d400c2816549d2
khlood-ebada <khloodebada97@gmail.com>
2024-01-29 15:12:20 -0800

    a

--- print_f.c
+++ print_f.c
@@ -1,32 +1,25 @@
 #include "main.h"
-
 /**
  * _printf - function that print chara and stringsn
  * @format: pointer
  * Return: chara_print
  */
-
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
 	va_list list_of_args;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 	va_start(list_of_args, format);
 	while (*format)
 		{
 		if (*format != '%')
 		{
-			write(1, format, 1)
+			write(1, format, 1);
 			chara_print++;
-		}
-		else
-		{
+		} else
 			format++;
-		}
 		if (*format == '\0')
 			break;
 		if (*format == '%')
@@ -45,13 +38,11 @@ int _printf(const char *format, ...)
 		{
 			char *str = va_arg(list_of_args, char*)
 				int str_len = 0;
-
 			while (str[str_len] != '\0')
 				str_len++;
 			write(1, str, str_len);
 			chara_print += str_len;
-		}
-		format++;
+		} format++;
 	}
 	va_end(list_of_args);
 	return (chara_print);
